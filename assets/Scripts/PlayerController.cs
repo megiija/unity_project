c6f3e60f46f3f4785bb7158b8f408681138f0d2c
megiija <megijasilinaxx@gmail.com>
2022-12-15 17:05:03 +0000

    test

---
+++ assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask collisionLayer;
+    public float moveSpeed;
+
+    private bool isMoving;
+    private Vector2 move;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            move.x = Input.GetAxisRaw("Horizontal");
+            move.y = Input.GetAxisRaw("Vertical");
+
+            // remove diagonal movement
+            if (move.x != 0) move.y = 0;
+
+            if (move != Vector2.zero)
+            {
+                animator.SetFloat("moveX", move.x);
+                animator.SetFloat("moveY", move.y);
+
+                var newPos = transform.position;
+                newPos.x += move.x;
+                newPos.y += move.y;
+
+
+                if (freeTile(new Vector3(newPos.x, newPos.y - 0.5f)))
+                {
+                    StartCoroutine(Move(newPos));
+                }
+            }
+        }
+
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    IEnumerator Move(Vector3 newPos)
+    {
+        isMoving = true;
+
+        while ((newPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, newPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = newPos;
+
+        isMoving = false;
+    }
+
+    private bool freeTile(Vector3 newPos)
+    {
+        return Physics2D.OverlapCircle(newPos, 0.2f, collisionLayer) == null;
+    } 
+}
