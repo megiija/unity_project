9ff48b2c6638a6c329641c28c8d5782a6ca9d937
megiija <megijasilinaxx@gmail.com>
2023-03-06 21:27:10 +0000

    new stuff

--- assets/Scripts/CameraController.cs
+++ assets/Scripts/CameraController.cs
@@ -6,10 +6,31 @@ public class CameraController : MonoBehaviour
 {
 
     public Transform player;
+    public BoxCollider2D mapBounds;
 
+    private float xMin, xMax, yMin, yMax;
+    private float camY, camX;
+    private float camOrthsize;
+    private float cameraRatio;
+    private Camera mainCam;
+
+    private void Start()
+    {
+        xMin = mapBounds.bounds.min.x;
+        xMax = mapBounds.bounds.max.x;
+        yMin = mapBounds.bounds.min.y;
+        yMax = mapBounds.bounds.max.y;
+        mainCam = GetComponent<Camera>();
+        camOrthsize = mainCam.orthographicSize;
+        cameraRatio = (xMax + camOrthsize) / 2.0f;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 0, -5);
+        camY = Mathf.Clamp(player.position.y, yMin + camOrthsize, yMax - camOrthsize);
+        camX = Mathf.Clamp(player.position.x, xMin + cameraRatio, xMax - cameraRatio);
+        this.transform.position = new Vector3(camX, camY, this.transform.position.z);
+
+
     }
 }
